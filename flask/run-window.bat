910ce88d67494363d28651562244cd6fd0ab6852
Sirapop Chatarupa <poap0403@hotmail.com>
2020-12-19 14:59:22 +0700

    ...

--- flask/run-window.bat
+++ flask/run-window.bat
@@ -1,4 +1,4 @@
 docker run -t -d -p 5000:5000 --name my-running-script --mount type=bind,source="%cd%",target=/usr/src/myapp -w /usr/src/myapp python:3  bash
 docker exec -it my-running-script pip install --no-cache-dir -r requirements.txt 
 docker exec -it my-running-script python3 app.py 
-pause
+docker rm -f my-running-script

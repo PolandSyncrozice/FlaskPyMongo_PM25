b93f17257799054142188572170cfa0f29f08eec
PolandSyncrozice <naratipediton@hotmail.com>
2020-12-20 20:02:03 +0700

    icon

--- flask/app.py
+++ flask/app.py
@@ -7,6 +7,7 @@ from os.path import join, dirname
 from dotenv import load_dotenv
 
 dotenv_path = join(dirname(__file__), '.env')
+print(dotenv_path)
 load_dotenv(dotenv_path)
 
 url = os.environ.get('DBURL') 
